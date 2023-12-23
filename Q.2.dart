89703146d48575af4dd2d69dcca82edc9cd5ea99
saboor94 <haidersuboor@gmail.com>
2023-12-23 15:56:24 +0500

    assign 2

--- Q.2.dart
+++ Q.2.dart
@@ -1,6 +1,6 @@
 //Q.2: remove all false values from below list by using removeWhere or retainWhere property.
 void main() {
-  List<String> name = ['John', 'Alice', 'eligible', 'Mike', 'Sarah', 'Tom'];
-  (name.retainWhere((Items) => Items[0].toUpperCase() == Items[0]));
+  List<String> name = ['John', 'Alice', 'eligible', 'Mike','sarah','Tom'];
+  (name.retainWhere((Items) => Items[0].toUpperCase() == Items[0])); 
   print(name);
 }
